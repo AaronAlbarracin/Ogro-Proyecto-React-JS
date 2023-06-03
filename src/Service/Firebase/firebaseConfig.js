dabbb1125cf931eb92b3af872bda458b0d903dd0
Aaron <aaronalbarracin22priv@gmail.com>
2023-06-03 05:30:24 -0600

    Actualizacion

--- src/Service/Firebase/firebaseConfig.js
+++ src/Service/Firebase/firebaseConfig.js
@@ -1,14 +1,15 @@
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDV6787fAg3_AeKnPExTvCR5NilOaydMAo",
-  authDomain: "ogro-29493.firebaseapp.com",
-  projectId: "ogro-29493",
-  storageBucket: "ogro-29493.appspot.com",
-  messagingSenderId: "677598588075",
-  appId: "1:677598588075:web:ee4937340aeb5dcc1adbee",
-  measurementId: "G-CC6ZGLKY38"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
